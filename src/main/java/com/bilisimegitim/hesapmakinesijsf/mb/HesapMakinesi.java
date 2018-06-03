1168d1c7963b723e3361df76f6d07f7dff078c21
BemDell1 <BemDell1@DESKTOP-6OQP37J>
2018-06-03 16:58:13 +0300

    HesapMakinesi ManagedBean oluşturuldu

---
+++ src/main/java/com/bilisimegitim/hesapmakinesijsf/mb/HesapMakinesi.java
@@ -0,0 +1,16 @@
+
+package com.bilisimegitim.hesapmakinesijsf.mb;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+
+@Named(value = "hesapMakinesi")
+@RequestScoped
+public class HesapMakinesi {
+
+  
+    public HesapMakinesi() {
+    }
+    
+}
